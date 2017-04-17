bcc3bbd6ecd1310b8827f436227a48c375a99803
theandrew168 <steveday168@gmail.com>
2017-04-17 15:15:29 -0500

    Add some material content to course samples

--- pga/pga/coursemanager.py
+++ pga/pga/coursemanager.py
@@ -6,8 +6,7 @@ from subprocess import Popen, PIPE
 from .dataAccess import DataAccess
 
 # Directory in which to store course / lesson data.
-# Should probably somewhere in /etc, /var, or /opt.
-COURSE_DIR = '.'
+COURSE_DIR = './static/courses'
 
 """
 coursemanager.py
@@ -45,12 +44,12 @@ def processCourseZip(in_mem_file):
     # Init database access
     db = DataAccess()
 
-    # TODO Get this data from a metadata file
     # Pull out course name and create its server directory
-    course_name = os.listdir(ZIP_TMP_DIR)[0]
-    course_path = os.path.join(COURSE_DIR, course_name)
-    course_path_tmp = os.path.join(ZIP_TMP_DIR, course_name)
-    course_order = 0
+    # Ex. 1_Course_Name
+    course_dir_name = os.listdir(ZIP_TMP_DIR)[0]
+    course_order, course_name = splitCourseDirectoryName(course_dir_name)
+    course_path = os.path.join(COURSE_DIR, course_dir_name)
+    course_tmp_path = os.path.join(ZIP_TMP_DIR, course_dir_name)
     if (os.path.exists(course_path)):
         print('[coursemanager] Modifying existing course')
     else:
@@ -60,7 +59,7 @@ def processCourseZip(in_mem_file):
     db.addCourse(course_name, course_order, course_path)
 
     # Create server directories for each lesson
-    course_lessons = os.listdir(os.path.join(ZIP_TMP_DIR, course_name))
+    course_lessons = os.listdir(os.path.join(ZIP_TMP_DIR, course_dir_name))
     for lesson_name in course_lessons:
         lesson_path = os.path.join(course_path, lesson_name)
         if (os.path.exists(lesson_path)):
@@ -69,12 +68,27 @@ def processCourseZip(in_mem_file):
             os.mkdir(os.path.join(course_path, lesson_name))
 
         # Copy all lesson content to server lesson directory
-        lesson_path_tmp = os.path.join(course_path_tmp, lesson_name)
-        for content in glob.glob(os.path.join(lesson_path_tmp, '*')):
+        lesson_tmp_path = os.path.join(course_tmp_path, lesson_name)
+        for content in glob.glob(os.path.join(lesson_tmp_path, '*')):
+            print(content)
+            if 'quiz' in content or 'materials' in content:
+                continue
             shutil.copy(content, lesson_path)
 
         # Add lesson info to database
-        lesson_path_html = os.path.join(lesson_path, lesson_name + '.html')
+        lesson_path_html = os.path.join(lesson_path, 'lesson.html')
         db.addLesson(course_name, lesson_name, lesson_path_html)
 
     return True, 'Course uploaded successfully: ' + course_name
+
+"""
+Split a course directory into its order and name.
+Ex. 1_Course_Name becomes (1, 'Course Name')
+
+@param course_dir_name Name of directory to split
+@return Tuple<Int, String> of course order and course name
+"""
+def splitCourseDirectoryName(course_dir_name):
+    course_order = int(course_dir_name.split('_')[0])
+    course_name = ' '.join(course_dir_name.split('_')[1:])
+    return course_order, course_name
