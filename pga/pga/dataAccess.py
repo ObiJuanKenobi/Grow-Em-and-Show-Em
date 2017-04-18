0509063d84735ae67c64678a9adbbd0f27a8d871
theandrew168 <steveday168@gmail.com>
2017-04-18 17:33:53 -0500

    Add quiz processing to unit uploads

--- pga/pga/dataAccess.py
+++ pga/pga/dataAccess.py
@@ -110,6 +110,11 @@ class DataAccess:
             lessons.append({"name": row[0], "path": row[1]})
         return lessons
 
+    def deleteQuiz(self, coursename):
+        self._cursor = self._connection.cursor()
+        self._cursor.execute("DELETE FROM Quiz_Questions WHERE Course_Name = %s;", [coursename])
+        self._cursor.execute("COMMIT")
+
     def addQuiz(self, coursename, questions):
         self._cursor = self._connection.cursor()
         for question in questions:
@@ -236,20 +241,14 @@ class DataAccess:
         self._cursor.execute("DELETE FROM Bed_Plans WHERE PlanID = %s", [planID])
         self._cursor.execute("COMMIT")
 
-#Class for passing quiz questions to the DB in a convenient object
+# Class for passing quiz questions to the DB in a convenient object
 class QuizQuestion:
-    _Text = None
-    _Answers = None
-
     def __init__(self):
-        _Text = ""
-        _Answers = []
+        self._Text = ""
+        self._Answers = []
 
-#Class for passing quiz answers to the DB in a convenient object
+# Class for passing quiz answers to the DB in a convenient object
 class QuizAnswer:
-    _Text = None
-    _IsCorrect = None
-
     def __init__(self):
-        _Text = ""
-        _IsCorrect = False
+        self._Text = ""
+        self._IsCorrect = False
